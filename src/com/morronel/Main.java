744dbab7a9c54af13b96b62631b06aaa91d54b21
Morronel <zahar.kogut@gmail.com>
2018-10-26 02:41:31 +0300

    ex5

--- src/com/morronel/Main.java
+++ src/com/morronel/Main.java
@@ -1,8 +1,16 @@
 package com.morronel;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	HashMap<String, String> map = new HashMap<String, String>();
+
+	map.put("matti", "mage");
+	map.put("mikael", "mixu");
+	map.put("arto", "arppa");
+
+        System.out.println(map.get("matti"));
     }
 }
